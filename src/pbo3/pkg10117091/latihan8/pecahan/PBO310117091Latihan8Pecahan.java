fbd7e2db9ec3096c6726a0259192695359b4cfda
Dida Handian <handiandida@gmail.com>
2018-09-23 19:47:46 +0700

    Pecahan

---
+++ src/pbo3/pkg10117091/latihan8/pecahan/PBO310117091Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan8.pecahan;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini berisi cara penulisan type data Pecahan
+ *                    pada program
+*/
+public class PBO310117091Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
